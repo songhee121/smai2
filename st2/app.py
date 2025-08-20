937e3b3bf6f4804d3ced5a738d0676f49a35ad5c
songhee121 <hehe9331@gmail.com>
2025-08-20 10:28:57 +0900

    250820

---
+++ st2/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+page_main=st.Page("main.py", title="Main Page")
+page_1=st.Page("p1.py", title="Page 1")
+page_2=st.Page("p2.py", title="Page 2")
+page_3=st.Page("p3.py", title="Page 3")
+
+page=st.navigation([page_main, page_1, page_2, page_3])
+
+page.run()
