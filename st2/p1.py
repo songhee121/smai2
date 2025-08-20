84650b25413ca7d8b2661d6b53dc67e2849a34ea
songhee121 <hehe9331@gmail.com>
2025-08-20 11:08:06 +0900

    250820

--- st2/p1.py
+++ st2/p1.py
@@ -1,4 +1,19 @@
 import streamlit as st
 
-st.title("Page 1")
+#Sidebar
 st.sidebar.markdown("Clicked Page 1")
+st.sidebar.markdown("Side Menu")
+st.sidebar.button("Click")
+st.sidebar.radio("성별",["여자", "남자"])
+#Page
+st.title("Page 1")
+st.markdown("Mark Down")
+st.header("Header")
+st.subheader("Sub Header")
+st.caption("Caption")
+st.code("""
+        def func():
+            print("OK")
+        def func2():
+            print("OK2")
+  """, language="python")
